e79e04ccf308aa14345eaccbf193edcd9d8a3d22
RAHUL SHARMA <rahulsharmaofficial356@gmail.com>
2021-08-26 11:49:02 +0530

    FunctionParameter

---
+++ FinalJava/src/FunPara.java
@@ -0,0 +1,19 @@
+public class FunPara{
+	public static void main(String[] args)
+	{
+		//function parameter
+	   //function call
+		addition(10,20);
+		addition(30,40);
+		System.out.println("bye");
+		
+	}
+	
+	
+	public static void addition (int a,int b)
+	{
+		int sum=a+b;
+		System.out.println(sum);
+		
+	}
+}

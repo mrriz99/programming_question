aba68c67c6874ef24d30a337af7e1fd47d84a828
RAHUL SHARMA <rahulsharmaofficial356@gmail.com>
2021-09-09 16:23:10 +0530

    question

--- FinalJava/src/String_Test.java
+++ FinalJava/src/String_Test.java
@@ -4,8 +4,8 @@ public class String_Test{
 		String str1="rahul";
 		String str2="rahul";
 		String str3=new String();
-		String str4=new String("rahulsharma");
-		String str5= new String("RahulSharma");
+		String str4=new String("DDD");
+		String str5= new String("CCCC");
 		
 		//== it checks the memory location of the value
 		System.out.println("1)"+str1==str2);
@@ -34,21 +34,21 @@ public class String_Test{
 		System.out.println("9)"+str4.compareToIgnoreCase(str5));
 		
 		//concat is use for the addit0in of the string
-		System.out.println(str1+str2);
+		System.out.println("10)"+str1+str2);
 		String str6=str1.concat(str5);
-		System.out.println(str6);
+		System.out.println("11)"+str6);
 		String str7="ramnath";
 		//join is use for the add some character in between two string
-		System.out.println(String.join("&",str1,str7));
+		System.out.println("12)"+String.join("&",str1,str7));
 		String str8="this isademoclass";
 		
-		System.out.println(str8.subSequence(3,9));
-		System.out.println(str8.substring(3));
-		System.out.println(str8.substring(3,9));
-		System.out.println(str8.replace("is","was"));
-		System.out.println(str8.replaceFirst("is","was"));
-		System.out.println(str8.replaceAll("is","was"));
-		System.out.println(str8.replaceAll("is(?)","was"));//? * .etc .... regex
+		System.out.println("13)"+str8.subSequence(3,9));
+		System.out.println("14)"+str8.substring(3));
+		System.out.println("15)"+str8.substring(3,9));
+		System.out.println("16)"+str8.replace("is","was"));
+		System.out.println("17)"+str8.replaceFirst("is","was"));
+		System.out.println("18)"+str8.replaceAll("is","was"));
+		System.out.println("19)"+str8.replaceAll("is(?)","was"));//? * .etc .... regex
 		
 		
 		
@@ -56,17 +56,18 @@ public class String_Test{
 		
 		
 		String str9="heLlo";
-		System.out.println(str9.indexOf("lo"));
-		System.out.println(str9.lastIndexOf('l'));
-		System.out.println(str9.contains("hel"));
-		System.out.println(str9.endsWith("0"));
-		System.out.println(str9.startsWith("he"));
-		System.out.println(str9.toUpperCase());
-		System.out.println(str9.toLowerCase());
+		System.out.println("20)"+str9.indexOf("lo"));
+		System.out.println("21)"+str9.lastIndexOf('l'));
+		System.out.println("22)"+str9.contains("hel"));
+		System.out.println("23)"+str9.endsWith("0"));
+		System.out.println("24)"+str9.startsWith("he"));
+		System.out.println("25)"+str9.toUpperCase());
+		System.out.println("26)"+str9.toLowerCase());
 		
 		int a=10;
 		String str10=String.valueOf(a);
-		System.out.println(str4+str10+str1);
+		System.out.println("27)"+str4+str10+str1);
+		System.out.println();
 		
 		
 		
